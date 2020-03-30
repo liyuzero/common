d08ad6155c4a5caf68bc41ac37d2f18a3b34d19b
”liyu“ <935732457@qq.com>
2020-03-30 13:45:04 +0800

    添加json

--- common/src/main/java/com/yu/lib/common/utils/PicUtils.kt
+++ common/src/main/java/com/yu/lib/common/utils/PicUtils.kt
@@ -6,14 +6,17 @@ import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.yu.lib.common.R
 
+object PicUtils {
 
-fun loadPic(imageView: ImageView, url: Any) {
-    if(url != imageView.tag) {
-        val drawableCrossFadeFactory = DrawableCrossFadeFactory.Builder(300).setCrossFadeEnabled(true).build()
-        Glide.with(imageView.context).load(url)
-            .centerCrop()
-            .placeholder(R.drawable.common_pic_default)
-            .transition(DrawableTransitionOptions.with(drawableCrossFadeFactory))
-            .into(imageView)
+    fun loadPic(imageView: ImageView, url: Any) {
+        if(url != imageView.tag) {
+            val drawableCrossFadeFactory = DrawableCrossFadeFactory.Builder(300).setCrossFadeEnabled(true).build()
+            Glide.with(imageView.context).load(url)
+                .centerCrop()
+                .placeholder(R.drawable.common_pic_default)
+                .transition(DrawableTransitionOptions.with(drawableCrossFadeFactory))
+                .into(imageView)
+        }
     }
+
 }
